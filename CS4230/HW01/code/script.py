6781af3197bb5fbef1d53c76a010fce9e4d96125
samlyme <sam@samly.me>
2025-09-09 12:47:54 -0700

    add code snippet

---
+++ CS4230/HW01/code/script.py
@@ -0,0 +1,18 @@
+
+from typing import Counter
+
+
+V = {"A", "B", "C", "D", "E"}
+E = {("A", "B"), ("A", "C"), ("B", "C"), ("B", "D"), ("D", "E")}
+
+graph = {v : [] for v in V}
+for e in E:
+    a, b = e
+    graph[a].append(b)
+    graph[b].append(a)
+
+degrees = Counter(len(edges) for edges in graph.values())
+
+for degree, count in degrees.items():
+    print(f"{count} nodes have degree {degree}")
+
