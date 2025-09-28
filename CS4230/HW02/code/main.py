820fb6abe23e7e80b06fb878fff26d5ddaece0a7
samlyme <sam@samly.me>
2025-09-27 18:19:40 -0700

    hw 2

--- CS4230/HW02/code/main.py
+++ CS4230/HW02/code/main.py
@@ -1,5 +1,17 @@
-def main():
-    print("Hello, World!")
+import networkx as nx
 
-if __name__ == "__main__":
-    main()
+
+edges = [(1, 2), (1, 3), (2, 3), (3, 4), (4, 5), (4, 6), (5, 6), (5, 7), (6, 7), (7, 8)]
+nodes = list(range(1, 9))
+
+graph = nx.Graph()
+graph.add_nodes_from(nodes)
+graph.add_edges_from(edges)
+
+print("density", nx.density(graph))
+
+print("clustering coef", nx.clustering(graph, 3))
+
+print("avg. path length", nx.average_shortest_path_length(graph))
+
+print("modularity", nx.algorithms.community.modularity(graph, [{1,2,3}, {4,5,6,7,8}]))
